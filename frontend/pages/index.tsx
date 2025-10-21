89d56cf64d3f324a0f6ae617a1300f16ed238b07
algazi2025 <algazi2002@hotmail.com>
2025-10-21 02:01:56 -0700

    Use Better Design

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,3 @@
-#import Link from 'next/link'
-#export default function Home() {
-#  return (
-#   <div style={{padding: '2rem', textAlign: 'center'}}>
-#      <h1>Used Car AI Platform</h1>
-#      <Link href="/seller/landing"><button>I'm a Seller</button></Link>
-#      <Link href="/dealer/landing"><button>I'm a Dealer</button></Link>
-#    </div>
-#  )
-#}
 import React from 'react';
 import { Car, TrendingUp, Users, CheckCircle } from 'lucide-react';
 
